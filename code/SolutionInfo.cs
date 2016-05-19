d60380703ea89de726d81a4be74ec783ca9c6f86
Paul Hatcher <paulh@atrios.co.uk>
2016-05-19 17:13:15 +0100

    Strong name the assemblies

--- code/SolutionInfo.cs
+++ code/SolutionInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("1.0.0")]
 [assembly: AssemblyFileVersionAttribute("1.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0 bcf5e7")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "1.0.0";
